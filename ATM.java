74dddd10f9b242550dfe3ca9730d7b0254b5e329
Atrij Mukherjee <146323563+Atrij10@users.noreply.github.com>
2023-11-01 23:33:54 +0530

    Create ATM.java

---
+++ ATM.java
@@ -0,0 +1,26 @@
+package io.github.atrij.corejava.project.atm;
+
+import java.io.IOException;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ATRIJ MUKHERJEE
+ */
+public class ATM extends OptionMenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        OptionMenu optionMenu = new OptionMenu();
+        
+        optionMenu.getLogin();
+    }
+    
+}
